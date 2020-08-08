04374753c3a0b99e97780053f9d27346412eb142
Normantas <kudzma.normantas@gmail.com>
2020-08-08 11:26:08 +0300

    Fixing color shift

--- js/editor.js
+++ js/editor.js
@@ -1,13 +1,37 @@
 "use strict";
 
+let sliderR = void 0
+let sliderG = void 0
+let sliderB = void 0
+
+let original = void 0
+
 function onOpenCvReady(){
+	sliderR = document.getElementById("shiftRed");
+	sliderR.oninput = deepfry;
+	sliderG = document.getElementById("shiftGreen");
+	sliderG.oninput = deepfry;
+	sliderB = document.getElementById("shiftBlue");
+	sliderB.oninput = deepfry;
+	
+	let inputElem = document.getElementById("input");
+	original = cv.imread(inputElem);
 	deepfry();
 }
 
 function deepfry(){
-	let inputElem = document.getElementById("input");
-	let mat = cv.imread(inputElem);
+	if (!original) { return; }
+	let mat = original.clone();
 	
-	let outputElem = document.getElementById("output");
-	cv.imshow(mat, outputElem);
+	// Apply color shift
+	let shiftR = sliderR.value & 0xff;
+	let shiftG = sliderG.value & 0xff;
+	let shiftB = sliderB.value & 0xff;
+	let colorShift = new cv.Mat(mat.rows, mat.cols, mat.type(), new cv.Scalar(shiftR, shiftG, shiftB, 255));
+	cv.add(mat, colorShift, mat);
+	colorShift.delete();
+	
+	//let outputElem = document.getElementById("output");
+	cv.imshow("output", mat);
+	mat.delete();
 }
